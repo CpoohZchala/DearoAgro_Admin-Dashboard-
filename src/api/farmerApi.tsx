09bd472b900adaf588551a5b8d04efa8d778baad
Cpooh <cpooh2001@gmail.com>
2025-06-18 09:48:29 +0530

    updated folder structure

--- src/api/farmerApi.tsx
+++ src/api/farmerApi.tsx
@@ -1,5 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/farmers';
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/';
 export interface FarmerData {
   fullName: string;
   mobileNumber: string;
